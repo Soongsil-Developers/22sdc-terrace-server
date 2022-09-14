64aeeda8d6035cd6ecfd51bd48799b5fe667b448
hum02 <ehdgma02@gmail.com>
2022-09-15 01:41:49 +0900

    unit테스트에 webdriver의존성 추가

--- src/test/kotlin/com/dev_camp/unit/auth/LoginUnitTest.kt
+++ src/test/kotlin/com/dev_camp/unit/auth/LoginUnitTest.kt
@@ -1,13 +1,15 @@
 package com.dev_camp.unit.auth
 
-import com.ninjasquad.springmockk.MockkBean
-import com.ninjasquad.springmockk.SpykBean
 import com.dev_camp.auth.dto.LoginRequestDto
 import com.dev_camp.auth.exception.LoginException
 import com.dev_camp.auth.service.AuthService
 import com.dev_camp.auth.tools.JwtTokenUtil
+import com.dev_camp.config.WebDriverConfig
 import com.dev_camp.unit.BaseUnitTest
+import com.dev_camp.user.domain.User
 import com.dev_camp.util.*
+import com.ninjasquad.springmockk.MockkBean
+import com.ninjasquad.springmockk.SpykBean
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
 import io.mockk.every
@@ -15,13 +17,18 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.openqa.selenium.WebDriver
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import java.util.Optional
+import java.util.*
+
 
 class LoginUnitTest : BaseUnitTest() {
 
+    var ctx: ApplicationContext = AnnotationConfigApplicationContext(WebDriverConfig::class.java)
+    private var driver: WebDriver= ctx.getBean("webDriver",WebDriver::class.java)
+
     private lateinit var authService: AuthService
-    private lateinit var driver: WebDriver
 
     @MockkBean
     private lateinit var encoder: BCryptPasswordEncoder
@@ -37,13 +44,13 @@ class LoginUnitTest : BaseUnitTest() {
     @DisplayName("로그인 성공")
     @Test
     fun login_Success() {
-        //로그인 구현 후 테스트해야함.
-/*        val user = User(id= USER_ID,name= NAME)
+        val user = User(id= USER_ID,name= NAME)
         every { encoder.matches(any(), any()) } returns true
-        val requestDto = LoginRequestDto(USER_ID, PASSWORD)
+        every { userRepository.save(any()) } returns getMockUser()
+        val requestDto = LoginRequestDto(studentId = USER_ID, password =  PASSWORD)
         val responseDto = authService.login(requestDto)
         jwtTokenUtil.isTokenExpired(responseDto.accessToken) shouldBe false
-        jwtTokenUtil.isTokenExpired(responseDto.refreshToken) shouldBe false*/
+        jwtTokenUtil.isTokenExpired(responseDto.refreshToken) shouldBe false
     }
 
     @DisplayName("로그인 실패 - 비밀번호 불일치")
@@ -60,8 +67,8 @@ class LoginUnitTest : BaseUnitTest() {
     @DisplayName("로그인 실패 - 존재하지 않는 학번")
     @Test
     fun login_FailIfWrongEmail() {
-        every { userRepository.findById(any()) } returns Optional.empty()
-        val requestDto = LoginRequestDto(USER_ID, PASSWORD)
+
+        val requestDto = LoginRequestDto(studentId = "wrongid", password = PASSWORD)
         val exception = shouldThrow<LoginException> { authService.login(requestDto) }
         exception.message shouldBe "학번 또는 비밀번호가 잘못되었습니다."
     }
