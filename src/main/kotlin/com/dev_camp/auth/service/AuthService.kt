5112f7165f53f0ff31922f66e24f25315ff73c7b
hum02 <ehdgma02@gmail.com>
2022-09-16 15:11:57 +0900

    유세인트의 alert처리

--- src/main/kotlin/com/dev_camp/auth/service/AuthService.kt
+++ src/main/kotlin/com/dev_camp/auth/service/AuthService.kt
@@ -10,6 +10,7 @@ import com.dev_camp.auth.tools.JwtTokenUtil
 import com.dev_camp.user.domain.User
 import com.dev_camp.user.domain.UserRepository
 import org.openqa.selenium.By
+import org.openqa.selenium.UnhandledAlertException
 import org.openqa.selenium.WebDriver
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
@@ -20,7 +21,7 @@ class AuthService(
     private val jwtTokenUtil: JwtTokenUtil,
     private val userRepository: UserRepository,
     private val encoder: BCryptPasswordEncoder,
-    private val driver:WebDriver
+    private val driver: WebDriver
 ) {
 
 
@@ -38,19 +39,25 @@ class AuthService(
     fun login(requestDto: LoginRequestDto): LoginResponseDto {
         driver.get("https://smartid.ssu.ac.kr/Symtra_sso/smln.asp?apiReturnUrl=https%3A%2F%2Fsaint.ssu.ac.kr%2FwebSSO%2Fsso.jsp")
 
-        // input id/password in u-saint login page
         driver.findElement(By.id("userid")).clear()
         driver.findElement(By.id("userid")).sendKeys(requestDto.studentId)
 
         driver.findElement(By.id("pwd")).clear()
         driver.findElement(By.id("pwd")).sendKeys(requestDto.password)
-        driver.findElement(By.id("pwd")).submit()
-        Thread.sleep(300)
 
-        System.out.print(driver.currentUrl)
+        try {
+            driver.findElement(By.id("pwd")).submit()
+            Thread.sleep(500)
+            System.out.print(driver.currentUrl)
+        } catch (e: UnhandledAlertException) {
+            driver.quit()
+            throw LoginException()
+        }
+
+
 
         if(driver.currentUrl.contains("https://saint.ssu.ac.kr/irj/portal")){
-            val user= User(id=requestDto.studentId,name="testName")
+            val user = User(id=requestDto.studentId,name="testName")
             //로그아웃 버튼 click
             driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/header/div[1]/div/div/button[2]")).click()
             userRepository.save(user)
@@ -60,7 +67,6 @@ class AuthService(
                 accessToken = jwtTokenUtil.generateAccessToken(user.id!!),
                 refreshToken = jwtTokenUtil.generateAccessToken(user.id!!),
             )
-
         }
         else {
             driver.quit()
