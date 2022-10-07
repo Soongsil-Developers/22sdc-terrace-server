6accef2c98a11614fb4aa4cc380a7b2484ee0644
hum02 <ehdgma02@gmail.com>
2022-10-07 17:27:39 +0900

    예약 userid로 찾는 쿼리추가

--- src/main/kotlin/com/dev_camp/reservation/domain/ReservationRepository.kt
+++ src/main/kotlin/com/dev_camp/reservation/domain/ReservationRepository.kt
@@ -1,9 +1,12 @@
 package com.dev_camp.reservation.domain
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface ReservationRepository : JpaRepository<Reservation, Int> {
     fun findAllByOrderByCreatedAtDesc(): List<Reservation>
+    @Query(value="select b from reservation b where reservation.user.user_id=?1")
+    fun findByUserId(user_id: String): List<Reservation>
 }
