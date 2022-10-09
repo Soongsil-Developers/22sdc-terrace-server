aadcd7c4238276d7ef33eb77c578343e0ba4875c
hum02 <ehdgma02@gmail.com>
2022-10-09 20:46:03 +0900

    findByUserId수정

--- src/main/kotlin/com/dev_camp/reservation/domain/ReservationRepository.kt
+++ src/main/kotlin/com/dev_camp/reservation/domain/ReservationRepository.kt
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Repository
 @Repository
 interface ReservationRepository : JpaRepository<Reservation, Int> {
     fun findAllByOrderByCreatedAtDesc(): List<Reservation>
+
+    //에러 발생할 수도 있음 추가 확인필요
     @Query(value="select b from reservation b where reservation.user.user_id=?1")
     fun findByUserId(user_id: String): List<Reservation>
 }
